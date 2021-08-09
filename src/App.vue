5dff90ba940b735d785c25ffddd2cc15aba2c181
Serven Maraghi <maraghiserven@gmail.com>
2021-08-09 01:15:45 -0400

    Commit work in progress

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
-  <button id="yo" class="yo" @click="toggleF = !toggleF">yo</button>
-  <button class="layers" @click="showLayers = !showLayers">Layers</button>
-  <div>{{ toggleF }}</div>
   <div id="map" />
 
-  <div class="layer-modal" v-if="showLayers">
-    <ul>
-      <li
-        v-for="layer in layerData"
-        :key="layer.id"
-        :class="{ active: layer.isActive }"
-        @click="toggleLayer(layer)"
-      >
-        {{ layer.id }}--{{ layer.enabled }}
-      </li>
-    </ul>
+  <div id="widget-container">
+    <button type="button" class="btn actions" @click="showModal('Actions')">
+      Actions
+    </button>
+
+    <button type="button" class="btn layers" @click="showModal('Layers')">
+      Layers
+    </button>
+
+    <button type="button" class="btn exp" @click="showModal('Export')">
+      Export
+    </button>
+  </div>
+
+  <div id="modal-container">
+    <Modal v-show="isModalVisible" @close="closeModal" v-if="showActions">
+      <template v-slot:header>
+        Actions
+      </template>
+
+      <template v-slot:body>
+        <button
+          @click="selectedTab = 'icon'"
+          :class="{ disabled: selectedTab === 'icon' }"
+        >
+          Icon
+        </button>
+        <button
+          @click="selectedTab = 'text'"
+          :class="{ disabled: selectedTab === 'text' }"
+        >
+          Text
+        </button>
+        <div v-if="selectedTab === 'icon'">
+          <select
+            v-model="selectedIcon"
+            @click="markerMode = true"
+            placeholder="Select Icon"
+          >
+            <option value="null" disabled hidden>Select Icon</option>
+            <option
+              v-for="option in options"
+              v-bind:key="option"
+              v-bind:value="option.value"
+            >
+              {{ option.text }}
+            </option>
+          </select>
+        </div>
+        <div v-if="selectedTab === 'text'">
+          <input type="text" v-model="selectedText" placeholder="Type here" />
+          <button @click="markerMode = true">Submit</button>
+        </div>
+      </template>
+    </Modal>
+
+    <Modal v-show="isModalVisible" @close="closeModal" v-if="showLayers">
+      <template v-slot:header>
+        Layers
+      </template>
+
+      <template v-slot:body>
+        <ul class="modal__layer">
+          <li
+            v-for="layer in layerData"
+            :key="layer.id"
+            :class="{ active: layer.isActive }"
+            @click="toggleLayer(layer)"
+          >
+            {{ layer.id }}--{{ layer.enabled }}
+          </li>
+        </ul>
+      </template>
+    </Modal>
+
+    <Modal v-show="isModalVisible" @close="closeModal" v-if="showExport">
+      <template v-slot:header>
+        Export
+      </template>
+
+      <template v-slot:body>
+        <div class="modal__export">
+          <p>Width</p>
+          <input type="text" v-model="exportHeight" />
+          <p>Height</p>
+          <input type="text" v-model="exportWidth" />
+          <button class="btn" @click="exportMap">Download</button>
+        </div>
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script>
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import html2canvas from "html2canvas";
+import Modal from "./components/Modal.vue";
 
 export default {
+  name: "App",
+  components: {
+    Modal,
+  },
   data() {
     return {
       loading: false,
@@ -30,14 +112,24 @@ export default {
         "pk.eyJ1Ijoic21hcmFnaGkiLCJhIjoiY2p6aDdqcHVzMG1oODNtcGs1MTUzZTRlYSJ9.tL_9KWMY8Ft3Mb1eLFizHw",
       center: [0, 0],
       map: {},
-      toggleF: true,
+      showActions: false,
       showLayers: false,
+      showExport: false,
+      exportHeight: 760,
+      exportWidth: 1080,
       layerData: [],
+      markerMode: false,
+      options: [],
+      selectedIcon: null,
+      selectedText: "",
+      selectedTab: "icon",
+      isModalVisible: false,
     };
   },
 
   mounted() {
     this.createMap();
+    this.loadData();
   },
 
   methods: {
@@ -62,27 +154,27 @@ export default {
       const nav = new mapboxgl.NavigationControl();
 
       this.map.on("click", (e) => {
-        if (this.toggleF === true) {
+        if (this.markerMode === true) {
           const coordinates = e.lngLat;
           const el = document.createElement("div");
 
-          const width = 50;
-          const height = 50;
+          if (this.selectedTab === "icon") {
+            const width = 50;
+            const height = 50;
 
-          //           .marker {
-          //   background-image: url("~@/assets/heart.svg");
-          // }
-          // el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
-          // el.style.backgroundImage = `url("/assets/heart.svg")`;
-          el.style.backgroundImage = `url("/assets/logo.png")`;
-          // this.svg = require(`resources/assets/images/svg/${this.name}.svg`);
-          // el.style.backgroundImage = this.svg;
-          el.className = "marker";
-          el.style.width = `${width}px`;
-          el.style.height = `${height}px`;
-          el.style.backgroundSize = "100%";
+            const yo = `/assets/icons/${this.selectedIcon}.svg`;
+            el.style.backgroundImage = `url(${yo})`;
+
+            el.className = "marker";
+            el.style.width = `${width}px`;
+            el.style.height = `${height}px`;
+            el.style.backgroundSize = "100%";
+          } else {
+            el.innerText = `${this.selectedText}`;
+          }
+
           new mapboxgl.Marker(el).setLngLat(coordinates).addTo(this.map);
-          console.log(el);
+          this.markerMode = false;
         }
       });
 
@@ -92,7 +184,6 @@ export default {
         this.layerData = this.map.getStyle().layers.map((l) => {
           return { ...l, ...{ enabled: true } };
         });
-        console.log(this.layerData);
       });
       return;
     },
@@ -107,6 +198,78 @@ export default {
         this.map.setLayoutProperty(layerName.id, "visibility", "visible");
       }
     },
+
+    exportMap() {
+      console.log("yo ma export");
+      const saveAs = (uri, filename) => {
+        console.log("yo ma friend");
+        var link = document.createElement("a");
+
+        if (typeof link.download === "string") {
+          link.href = uri;
+          link.download = filename;
+
+          //Firefox requires the link to be in the body
+          document.body.appendChild(link);
+
+          //simulate click
+          link.click();
+
+          //remove the link when done
+          document.body.removeChild(link);
+        } else {
+          window.open(uri);
+        }
+      };
+
+      const cnvs = document.querySelector("#map");
+      html2canvas(cnvs, {
+        width: this.exportWidth,
+        height: this.exportHeight,
+      }).then((canvas) => {
+        saveAs(canvas.toDataURL(), "map.png");
+      });
+    },
+
+    loadData() {
+      // wouuld forloop through assets
+      this.options = [
+        { text: "Heart", value: "heart" },
+        { text: "BBQ", value: "bbq" },
+        { text: "Star", value: "star" },
+      ];
+    },
+
+    showModal(type) {
+      this.showActions = false;
+      this.showLayers = false;
+      this.showExport = false;
+
+      if (type === "Actions") {
+        this.showActions = true;
+      } else if (type === "Layers") {
+        this.showLayers = true;
+      } else if (type === "Export") {
+        this.showExport = true;
+      }
+
+      this.isModalVisible = true;
+    },
+
+    closeModal() {
+      this.isModalVisible = false;
+      this.showActions = false;
+      this.showLayers = false;
+      this.showExport = false;
+    },
+
+    activeTab(tab) {
+      if (tab === "icon") {
+        this.selectedTab === "icon";
+      } else {
+        this.selectedTab === "text";
+      }
+    },
   },
 };
 </script>
@@ -116,7 +279,31 @@ export default {
   height: 100vh;
 }
 
-.layer-modal {
+.modal {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 50;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 500px;
+  background: #fff;
+  box-shadow: 0 6px 8px 0 rgba(44, 8, 8, 0.5);
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.7s ease;
+}
+/***
+.modal__layer {
   position: absolute;
   top: 0;
   bottom: 0;
@@ -126,7 +313,58 @@ export default {
   color: blue;
 }
 
+.modal__export {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 200px;
+  z-index: 2;
+  background: gray;
+  color: blue;
+}
+
+.actions-modal {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 200px;
+  z-index: 2;
+  background: gray;
+  color: blue;
+}
+
+**/
 .active {
   color: red;
+}
+
+.disabled {
+  background: gray;
+}
+
+#widget-container {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  z-index: 10;
+  top: 0;
+  bottom: 0;
+  max-height: 200px;
+  justify-content: space-evenly;
+}
+
+.btn {
+  font-weight: 500;
+  line-height: 1.37;
+  letter-spacing: 0.2px;
+  text-align: center;
+  font-size: 14px;
+  display: inline-block;
+  padding: 8px 16px;
+  text-align: center;
+  background-color: #415572;
+  color: #fff;
+  border: 1px solid #3c484d;
+  border-radius: 2px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,102 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div
+        class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header class="modal-header" id="modalTitle">
+          <slot name="header">
+            This is the default title!
+          </slot>
+          <button type="button" class="btn-close" @click="close">
+            x
+          </button>
+        </header>
+
+        <section class="modal-body" id="modalDescription">
+          <slot name="body">
+            This is the default body!
+          </slot>
+        </section>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+
+.modal-header {
+  position: relative;
+  border-bottom: 1px solid #eeeeee;
+  color: #4aae9b;
+  justify-content: space-between;
+}
+
+.modal-footer {
+  border-top: 1px solid #eeeeee;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  font-size: 20px;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #4aae9b;
+  background: transparent;
+}
+
+.btn-green {
+  color: white;
+  background: #4aae9b;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+</style>
